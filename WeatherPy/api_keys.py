f6b3909f40dc79f698eea3f6eb43e268ea569011
EMDeLeon <58098771+EMDeLeon@users.noreply.github.com>
2020-02-05 18:23:35 -0500

    delete key

--- WeatherPy/api_keys.py
+++
@@ -1,6 +0,0 @@
-# OpenWeatherMap API Key
-weather_api_key = "7c625f2206cc5f83d171aa76af511676"
-weather_api_key2= "9d349025062367fe19329889d6c74ea8"
-
-# Google API Key
-g_key = "AIzaSyDl5YMBzcErZ5V8bfjecD-erPueAoHpya8"
